dacdb8841c2e559e512fceaf5b4501a2bfdc8700
MadhuraPande <MadhuraPande@users.noreply.github.com>
2015-04-13 11:27:45 +0530

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,26 +4,22 @@ and print "fizzbuzz" if divisible by both.*/
 
 function print() {
 
-    //var outputarea = document.getElementById("output-area");
-    //while (outputarea.hasChildNodes()) {
-    //    outputarea.removeChild(outputarea.lastChild);
-    //}
-    var startNumber = document.getElementById("number1").value;
-    var endNumber = document.getElementById("number2").value;
-    for (var i = startNumber; i <= endNumber; i++) {
+    var startNumber = document.getElementById("number1").value;     //get first number entered by user
+    var endNumber = document.getElementById("number2").value;       //get second number entered by user
+    for (var i = startNumber; i <= endNumber; i++) {                
         var node = document.createElement("p");
-        if (i % 15 == 0) {
+        if (i % 15 == 0) {                                              //if divisible by 5 and 3 both print fizzbuzz
             node.appendChild(document.createTextNode(i + ":fizzbuzz"));
             document.getElementById("output-area").appendChild(node);
             continue;
         }
         if (i % 5 == 0) {
-            node.appendChild(document.createTextNode(i + ":buzz"));
+            node.appendChild(document.createTextNode(i + ":buzz"));     //if divisible only by 5 print buzz
             document.getElementById("output-area").appendChild(node);
             continue;
         }
         if (i % 3 == 0) {
-            node.appendChild(document.createTextNode(i + ":fizz"));
+            node.appendChild(document.createTextNode(i + ":fizz"));         //if divisible only by 3 print fizz
             document.getElementById("output-area").appendChild(node);
             continue;
         }
