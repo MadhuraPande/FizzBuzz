93e1007ca0e4b474818565abeb72ec026840792d
MadhuraPande <MadhuraPande@users.noreply.github.com>
2015-05-03 16:22:43 +0530

    Added divisibility for number '7'
    The code is now more flexible. All numbers for divisibility test are stored as an array. Therefore, any new number can be accomodated.
    Also, the strings to be print for each number are in a list. Therefore, it is flexible in a sense it can add new/ change existing/ exchange strings without changing actual logic of calculating

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,29 +1,34 @@
 //javascript program - FizzBuzz
-/*Descirption - Given a set of numbers, for each number, print "fizz" if divisible by 3, print "buzz" if divisible by 5 
-and print "fizzbuzz" if divisible by both.*/
+/*Descirption - Given a set of numbers, for each number, check divisibility for some numbers and print respective 
+string associated with that number*/
 
-function print(printNumber, printString){
+
+//print output for each number
+function print(printNumber, printString) {
     var node = document.createElement("p");
-    node.appendChild(document.createTextNode(printNumber + " :" + printString));
+    node.appendChild(document.createTextNode(printNumber + ": " + printString));
     document.getElementById("output-area").appendChild(node);
 }
 
+//divisibility test
 function calculate() {
-
-    var startNumber = document.getElementById("number1").value;     //get first number entered by user
-    var endNumber = document.getElementById("number2").value;       //get second number entered by user
-    document.getElementById("output-area").innerHTML = "";          //clear initial output before each new request
-    for (var i = startNumber; i <= endNumber; i++) {                
-        if (i % 15 == 0) {                                              //if divisible by 5 and 3 both print fizzbuzz
-            print(i,"fizzbuzz");
-            continue;
+    var startNumber = document.getElementById("number1").value; //get first number entered by user
+    var endNumber = document.getElementById("number2").value; //get second number entered by user
+    
+    document.getElementById("output-area").innerHTML = "";  //clear page at every new request
+    
+    var numberList = [3, 5, 7];                 //list of numbers for divisibility test
+    var stringList = ["fizz","buzz","tuzz"];    //list of print strings for respective numbers
+    
+    for (var i = startNumber; i <= endNumber; i++) {
+        var outputString = "";
+        for (var j = 0; j < numberList.length; j++) {
+            if (i % numberList[j] == 0) {
+                outputString += stringList[j];              //since each number is associated with particular string, 
+            }                                       //order of strings is important.
         }
-        if (i % 5 == 0) {
-            print(i,"buzz");
-            continue;
+        if (outputString != "") {           //print only those numbers which are divisible by atleast one number from numberlist
+            print(i, outputString);
         }
-        if (i % 3 == 0) {
-            print(i,"fizz");
-        }
-    }
+   }
 }
